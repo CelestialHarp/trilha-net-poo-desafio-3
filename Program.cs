02ee19d74cf83f3907a68a89f06455edd2cb0db0
CelestialHarp <celestialharp@protonmail.com>
2025-11-24 11:03:02 -0300

    Solve the challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using System.Xml;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Por via das dúvidas, implementei os mesmos testes que o professor realizou no vídeo.
+Console.WriteLine("Testando no Nokia:");
+Smartphone nokia = new Nokia(numero: "321436541", modelo: "Modelo GX", imei: "3136154351", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando no iPhone:");
+Smartphone iphone = new Iphone(numero: "68756456", modelo: "Modelo 0", imei: "564365354", memoria: 64);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Calculadora");
